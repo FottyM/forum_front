a30c8d5210ed8706afce1e13ca9ec9216dcfadbd
Fortunat Mutunda <fotimutunda@gmail.com>
2017-09-08 23:07:13 +0300

    basic view questions list

--- src/components/Question/Questions.vue
+++ src/components/Question/Questions.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-    Questions
-  </div>
+<v-layout>
+  <v-flex>
+    <v-card>
+      <v-card-title>
+        Title
+      </v-card-title>
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate delectus eos explicabo molestias numquam quaerat rerum sapiente unde! A animi consectetur dolores ipsa minus, natus quia. Error hic obcaecati officia.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="primary">Press Hard</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</v-layout>
 </template>
 <script>
   export default {
-    name: 'Questions'
+    name: 'Questions',
+    data(){
+      return{
+
+      }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    }
   }
 
 </script>
---
+++ src/store/store.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+
+  state: {
+    currentUser: null,
+    questions: null,
+    answers: null
+  },
+
+  mutations: {
+    SET_USER(state, user) {
+      state.currentUser = user
+    },
+    SET_CURRENT_CHANNEL(state, channel) {
+      state.currentChannel = channel
+    }
+  },
+
+  actions: {
+    setUser({commit}, user) {
+      commit('SET_USER', user)
+    },
+    setCurrentChannel({commit}, channel) {
+      commit('SET_CURRENT_CHANNEL', channel)
+    },
+
+  },
+
+  getters: {
+    currentUser: state => state.currentUser,
+    currentChannel: state => state.currentChannel,
+  }
+
+});
+
+export default store;
